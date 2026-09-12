816550fdad8fdff699768260501ab29df4c6efef
Harsh verma <lookharshverma@gmail.com>
2026-09-12 19:18:32 +0530

    Add files via upload

---
+++ test-server.js
@@ -0,0 +1,9 @@
+﻿import express from 'express';
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req, res) => res.send('<h1>DealPulse Server is Running!</h1>'));
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
